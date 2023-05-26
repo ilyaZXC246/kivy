bbe5f0a1b48cb7198d6204449e1ff1ae36b1bcd8
ilya <irotgan777@mail.ru>
2023-05-26 13:13:25 +0500

    v1

--- kiv.py
+++ kiv.py
@@ -2,14 +2,18 @@
 # os.environ['KIVY_NO_CONSOLELOG'] = 'hide'
 
 from kivy.app import App
+from kivy.graphics import *
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+from random import randint as rand
 
 
 PASSWORD = '1'
+points = []
 
 
 class Applick(App):
@@ -22,17 +26,17 @@ class Applick(App):
 
         self.sm = ScreenManager()
         self.main_screen = MainScreen(name='main_screen')
-        self.left_screen = ChildScreen(name='left_screen', text='левый экран')
-        self.up_screen = ChildScreen(name='up_screen', text='верхний экран')
-        self.right_screen = ChildScreen(name='right_screen', text='правый экран')
-        self.down_screen = ChildScreen(name='down_screen', text='нижний экран')
+        # self.left_screen = ChildScreen(name='left_screen', text='левый экран')
+        self.points_screen = PointsScreen(name='points_screen')
+        # self.right_screen = ChildScreen(name='right_screen', text='правый экран')
+        # self.down_screen = ChildScreen(name='down_screen', text='нижний экран')
         self.protected_screen = ProtectedScreen(name='protected_screen')
 
         self.sm.add_widget(self.main_screen)
-        self.sm.add_widget(self.left_screen)
-        self.sm.add_widget(self.up_screen)
-        self.sm.add_widget(self.right_screen)
-        self.sm.add_widget(self.down_screen)
+        # self.sm.add_widget(self.left_screen)
+        self.sm.add_widget(self.points_screen)
+        # self.sm.add_widget(self.right_screen)
+        # self.sm.add_widget(self.down_screen)
         self.sm.add_widget(self.protected_screen)
 
         return self.sm
@@ -65,20 +69,26 @@ class MainScreen(Screen):
         self.add_widget(self.main_layout)
 
 
-class ChildScreen(Screen):
-    """КОСВЕНЫЕ ЭКРАНЫ"""
+class PointsScreen(Screen):
+    """Экран с точками"""
 
-    def __init__(self, text, **kwargs):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.main_layout = BoxLayout(orientation='vertical', padding=8, spacing=8)
+        self.main_layout = BoxLayout(orientation='vertical')
+        self.main_layout.can = Widget()
+        self.main_layout.layout = BoxLayout(orientation='horizontal', size_hint=(1, None))
+        self.main_layout.layout.plus100_btn = PlusButton(pl=100, text='+100')
+        self.main_layout.layout.plus500_btn = PlusButton(pl=500, text='+500')
+        self.main_layout.layout.reset_btn = ResetButton(text='reset')
+        self.main_layout.layout.back_btn = NavButton(nav='main_screen', text='Назад', clear=True)
 
-        self.main_layout.text = Label(text=text)
-        self.main_layout.btn = NavButton(nav='main_screen', text='назад на главный')
-
-        self.main_layout.add_widget(self.main_layout.text)
-        self.main_layout.add_widget(self.main_layout.btn)
-
+        self.main_layout.add_widget(self.main_layout.can)
+        self.main_layout.layout.add_widget(self.main_layout.layout.plus100_btn)
+        self.main_layout.layout.add_widget(self.main_layout.layout.plus500_btn)
+        self.main_layout.layout.add_widget(self.main_layout.layout.reset_btn)
+        self.main_layout.layout.add_widget(self.main_layout.layout.back_btn)
+        self.main_layout.add_widget(self.main_layout.layout)
         self.add_widget(self.main_layout)
 
 
@@ -112,14 +122,17 @@ class ProtectedScreen(Screen):
 class NavButton(Button):
     """Навигационные кнопки"""
 
-    def __init__(self, nav, **kwargs):
+    def __init__(self, nav, clear=False, **kwargs):
         super().__init__(**kwargs)
         self.nav = nav
-
+        self.clear = clear
         self.on_press = self.next
 
     def next(self):
         """Переключает экран при нажатии"""
+
+        if self.clear:
+            app.points_screen.main_layout.can.canvas.clear()
 
         if self.nav == 'up_screen':
             app.position = 'up'
@@ -149,11 +162,54 @@ class NavButton(Button):
             app.main_screen.manager.current = 'main_screen'
 
         if self.nav == 'apply':
+
             if app.protected_screen.main_layout.text_input.text == PASSWORD:
-                app.main_screen.manager.current = app.position + '_screen'
+
+                if app.position == 'up':
+                    app.main_screen.manager.current = 'points_screen'
+
+                elif app.position == 'down':
+                    pass
+
+                elif app.position == 'left':
+                    pass
+
+                elif app.position == 'right':
+                    pass
+
             else:
                 app.protected_screen.main_layout.txt.text = 'Неверный пароль'
                 app.protected_screen.main_layout.txt.color = 'red'
+
+
+class PlusButton(Button):
+    """КНОПКИ ДОБАЛЯЮЩИЕ ТОЧКИ"""
+
+    def __init__(self, pl, **kwargs):
+        super().__init__(**kwargs)
+        self.pl = pl
+        self.on_press = self.plus_points
+
+    def plus_points(self):
+        """Добавление"""
+        with app.points_screen.main_layout.can.canvas:
+            for i in range(self.pl):
+                R, G, B, A = rand(0, 255) / 256, rand(0, 255) / 256, rand(0, 255) / 256, 1
+                Color(R, G, B, A)
+                Rectangle(pos=(rand(0, 600), rand(0, 600)), size=(10, 10))
+
+
+class ResetButton(Button):
+    """КНОПКА УБИРАЕТ ВСЕ ТОЧКИ"""
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.on_press = self.reset
+
+    @staticmethod
+    def reset():
+        """очистка холста"""
+        app.points_screen.main_layout.can.canvas.clear()
 
 
 if __name__ == '__main__':
